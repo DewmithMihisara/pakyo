b685612275ac24298ff34487f2017c5bb1a669e1
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2025-04-03 15:15:57 +0530

    first commit

---
+++ src/main/java/com/hcodeSolutions/Main.java
@@ -0,0 +1,12 @@
+package com.hcodeSolutions;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2025-04-03
+ * @since ${VERSION}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/com/hcodeSolutions/controller/MainFormController.java
@@ -0,0 +1,9 @@
+package com.hcodeSolutions.controller;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2025-04-03
+ * @since 0.0.1
+ */
+public class MainFormController {
+}
