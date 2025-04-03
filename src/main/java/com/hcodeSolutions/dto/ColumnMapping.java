233f05a9cff184487a24d962d6125501f7e26ebb
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2025-04-03 16:25:06 +0530

    remove lombock

--- src/main/java/com/hcodeSolutions/dto/ColumnMapping.java
+++ src/main/java/com/hcodeSolutions/dto/ColumnMapping.java
@@ -1,18 +1,46 @@
 package com.hcodeSolutions.dto;
 
-import lombok.*;
 
 /**
  * @author Dewmith Mihisara
  * @date 2025-04-03
  * @since 0.0.1
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ColumnMapping {
     private String excelColumn;
     private String dbColumn;
     private String dataType;
+
+    public ColumnMapping() {
+    }
+
+    public ColumnMapping(String excelColumn, String dbColumn, String dataType) {
+        this.excelColumn = excelColumn;
+        this.dbColumn = dbColumn;
+        this.dataType = dataType;
+    }
+
+    public String getDbColumn() {
+        return dbColumn;
+    }
+
+    public void setDbColumn(String dbColumn) {
+        this.dbColumn = dbColumn;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public String getExcelColumn() {
+        return excelColumn;
+    }
+
+    public void setExcelColumn(String excelColumn) {
+        this.excelColumn = excelColumn;
+    }
 }
